6ecd215b280cc9112e39ae84efcf036dfcd92958
DeliaPestana <deliapestana@hotmail.com>
2023-03-29 20:55:05 +0100

    Fin de clase

---
+++ src/com/hedima/presentacion/ProbaarEstructuraIf.java
@@ -0,0 +1,24 @@
+package com.hedima.presentacion;
+
+public class ProbaarEstructuraIf {
+    public static void main(String[] args) {
+
+        //En base a una variable decir si es mayor de edad o menor
+        //Declaro la variable
+        int edad = 18;
+
+
+        if (edad >= 18) {
+            System.out.println("Es mayor de edad");
+        } else {
+            System.out.println("Es menor de edad");
+        }
+        //Declarar una variable y decir si es un número positivo o negativo
+
+
+
+        //Declarar una variable y decir si es un número par o impar
+
+
+    }
+}
